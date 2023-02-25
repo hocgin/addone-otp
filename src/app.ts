98928c9a827a36ffe12329685ce6db62d4b201bc
hocgin <hocgin@gmail.com>
2023-02-26 00:23:07 +0800

    TASK#调整

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,5 @@
-import { getAllLocales } from 'umi';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import '@/request.config';
 
 dayjs.extend(relativeTime);
-
-// 国际化配置
-getAllLocales().forEach((locale) => {
-});
-
-// 全局状态配置
-export async function getInitialState() {
-  return {
-    author: 'hocgin',
-  };
-}
