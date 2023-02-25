a74f261b2651af04f656b31ecdd0287b6bd00d80
hocgin <hocgin@gmail.com>
2023-02-26 00:25:35 +0800

    TASK#调整

--- src/router.config.ts
+++ src/router.config.ts
@@ -1,10 +1,1 @@
-export default [
-  {
-    path: '/',
-    component: '@/layouts/BasicLayout',
-    routes: [
-      {path: '/', component: '@/pages/index'},
-    ],
-  },
-  {component: '@/pages/404'},
-];
+export default [];
